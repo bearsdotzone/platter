10e6c83b290259a8e149574f79d9544a1c38d015
Bears Abney <bears@bears.zone>
2021-07-28 15:56:17 -0500

    1.17.1 Update in Progress

--- src/main/java/com/abneyonline/platter/tile/BirchPlatterTile.java
+++ src/main/java/com/abneyonline/platter/tile/BirchPlatterTile.java
@@ -1,11 +1,13 @@
 package com.abneyonline.platter.tile;
 
 import com.abneyonline.platter.Registration;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class BirchPlatterTile extends PlatterTile {
 
-    public BirchPlatterTile()
+    public BirchPlatterTile(BlockPos blockPos, BlockState blockState)
     {
-        super(Registration.birch_platter_tile.get());
+        super(Registration.birch_platter_tile.get(), blockPos, blockState);
     }
 }
--- src/main/java/com/abneyonline/platter/tile/StonePlatterTile.java
+++ src/main/java/com/abneyonline/platter/tile/StonePlatterTile.java
@@ -1,11 +1,13 @@
 package com.abneyonline.platter.tile;
 
 import com.abneyonline.platter.Registration;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class StonePlatterTile extends PlatterTile {
-    public StonePlatterTile()
+    public StonePlatterTile(BlockPos blockPos, BlockState blockState)
     {
-        super (Registration.stone_platter_tile.get());
+        super (Registration.stone_platter_tile.get(), blockPos, blockState);
         tickForAnimals = false;
     }
 }
