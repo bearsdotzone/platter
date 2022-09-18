e868152e05563980ee95ac1e1f5043dab5ea35d7
Elijah Abney <abney42@gmail.com>
2022-09-18 10:22:30 -0500

    create 1.19

--- src/main/java/com/abneyonline/platter/PlatterMod.java
+++ src/main/java/com/abneyonline/platter/PlatterMod.java
@@ -5,9 +5,8 @@ import com.abneyonline.platter.network.SyncHandler;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -16,13 +15,11 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-//import net.minecraftforge.fmlclient.registry.ClientRegistry;
-//import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.stream.Collectors;
+
+//import net.minecraftforge.fmlclient.registry.ClientRegistry;
+//import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("platter")
@@ -99,12 +96,12 @@ public class PlatterMod
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-//            LOGGER.info("HELLO from Register Block");
-        }
-    }
+//    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+//    public static class RegistryEvents {
+//        @SubscribeEvent
+//        public static void onBlocksRegistry(final RegisterEvent.Register<Block> blockRegistryEvent) {
+//            // register a new block here
+////            LOGGER.info("HELLO from Register Block");
+//        }
+//    }
 }
