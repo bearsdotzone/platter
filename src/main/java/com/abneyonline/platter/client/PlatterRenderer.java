9f8fccf01ac20d9518c3e3474e0484b1a6531b1c
Bears Abney <bears@bears.zone>
2023-05-20 13:34:20 -0500

    Add feature and configuration for render eccentricity, if enabled, the items on the platter shall render slightly askew.

--- src/main/java/com/abneyonline/platter/client/PlatterRenderer.java
+++ src/main/java/com/abneyonline/platter/client/PlatterRenderer.java
@@ -1,5 +1,6 @@
 package com.abneyonline.platter.client;
 
+import com.abneyonline.platter.Config;
 import com.abneyonline.platter.tile.PlatterTile;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
@@ -10,6 +11,7 @@ import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.item.ItemStack;
+import org.joml.Random;
 
 import java.util.ArrayDeque;
 
@@ -23,6 +25,7 @@ public class PlatterRenderer implements BlockEntityRenderer<PlatterTile> {
     public void render(PlatterTile tileEntityIn, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int combinedLightIn, int combinedOverlayIn) {
         matrixStackIn.pushPose();
 
+        Random r = new Random(tileEntityIn.hashCode());
 
         Quaternionf pose = new Quaternionf();
         pose.rotateX((float) Math.toRadians(90f));
@@ -43,8 +46,19 @@ public class PlatterRenderer implements BlockEntityRenderer<PlatterTile> {
 
         while (items.size() > 0) {
             ItemStack itemIn = items.pop();
+            if(Config.PLATTER_ECCENTRICITY_ENABLED.get()) {
 
+                boolean addEccentricity = r.nextInt(2) == 1;
 
+                if(addEccentricity)
+                {
+                    matrixStackIn.mulPose(new Quaternionf().rotateLocalZ((float) Math.toRadians(r.nextInt(Config.PLATTER_ECCENTRICITY_VALUE.get()))));
+                }
+                else
+                {
+                    matrixStackIn.mulPose(new Quaternionf().rotateLocalZ((float) Math.toRadians(r.nextInt(Config.PLATTER_ECCENTRICITY_VALUE.get() )* -1)));
+                }
+            }
 
             matrixStackIn.translate(0f, 0.0f, -0.063f);
             if (!itemIn.isEmpty()) {
