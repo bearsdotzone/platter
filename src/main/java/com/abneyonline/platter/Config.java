11e217e1284b25abe756704df907b504b0397ec4
Elijah Abney <abney42@gmail.com>
2023-05-20 13:34:20 -0500

    Add feature and configuration for render eccentricity, if enabled, the items on the platter shall render slightly askew.

--- src/main/java/com/abneyonline/platter/Config.java
+++ src/main/java/com/abneyonline/platter/Config.java
@@ -12,6 +12,8 @@ public class Config {
     public static ForgeConfigSpec.IntValue PLATTER_SLOTS;
     public static ForgeConfigSpec.IntValue PLATTER_RADIUS;
     public static ForgeConfigSpec.IntValue PLATTER_PERIOD;
+    public static ForgeConfigSpec.BooleanValue PLATTER_ECCENTRICITY_ENABLED;
+    public static ForgeConfigSpec.IntValue PLATTER_ECCENTRICITY_VALUE;
 
 
     static {
@@ -23,6 +25,8 @@ public class Config {
         PLATTER_SLOTS = COMMON_BUILDER.comment("Item slots for the platter [default: 9]").defineInRange("platter_slots", 9, 0, Integer.MAX_VALUE);
         PLATTER_RADIUS = COMMON_BUILDER.comment("Radius for the platter to feed [default: 5]").defineInRange("platter_radius", 5, 0, 32);
         PLATTER_PERIOD = COMMON_BUILDER.comment("Time (in seconds) to wait between platter searches [default: 60]").defineInRange("platter_period", 60, 0, Integer.MAX_VALUE);
+        PLATTER_ECCENTRICITY_ENABLED = COMMON_BUILDER.comment("Shall platters render items slightly askew? [default: true]").define("platter_eccentricity_enabled", true);
+        PLATTER_ECCENTRICITY_VALUE = COMMON_BUILDER.comment("Should eccentricity be enabled, by how many degrees shall the items render askew each layer? [default: 10]").defineInRange("platter_eccentricity_value", 10, 0, Integer.MAX_VALUE);
 
         COMMON_BUILDER.pop();
 
