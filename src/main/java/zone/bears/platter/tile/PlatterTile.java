0ce88966daa76dc5759b529b5c339dda4c109580
bearsdotzone <bears@bears.zone>
2026-01-05 22:43:21 -0600

    Default for optional
    Signed-off-by: bearsdotzone <bears@bears.zone>

--- src/main/java/zone/bears/platter/tile/PlatterTile.java
+++ src/main/java/zone/bears/platter/tile/PlatterTile.java
@@ -53,7 +53,7 @@ public class PlatterTile extends BlockEntity {
     @Override
     protected void loadAdditional(CompoundTag pTag, HolderLookup.Provider pRegistries) {
         super.loadAdditional(pTag, pRegistries);
-        itemStackHandler.deserializeNBT(pRegistries, pTag.getCompound("inv").get());
+        itemStackHandler.deserializeNBT(pRegistries, pTag.getCompound("inv").orElse(new CompoundTag()));
     }
 
     @Override
@@ -148,7 +148,7 @@ public class PlatterTile extends BlockEntity {
     @Override
     public void handleUpdateTag(CompoundTag tag, HolderLookup.Provider lookupProvider) {
         super.handleUpdateTag(tag, lookupProvider);
-        itemStackHandler.deserializeNBT(lookupProvider, tag.getCompound("Items").get());
+        itemStackHandler.deserializeNBT(lookupProvider, tag.getCompound("Items").orElse(new CompoundTag()));
     }
 
 
